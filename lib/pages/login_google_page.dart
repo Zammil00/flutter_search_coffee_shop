c544bc405f4a73f931c4b16c6d15bd004cfee916
INI_LABIL <115417258+Zammil00@users.noreply.github.com>
2025-02-12 15:21:21 +0700

    Update Login Page

---
+++ lib/pages/login_google_page.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class LoginGooglePage extends StatelessWidget {
+  const LoginGooglePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Image.asset(
+              "assets/images/coffee.jpg",
+              fit: BoxFit.cover,
+              height: double.infinity,
+              width: double.infinity,
+              alignment: Alignment.center,
+            ),
+            Positioned(
+              top: 50,
+              child: Text(
+                "AYO NGOPI",
+                style: GoogleFonts.lato(
+                  color: Colors.white,
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 50,
+              left: 10,
+              right: 10,
+              child: Card(
+                color: Colors.black.withOpacity(0.3),
+                elevation: 8.0,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8)),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Text(
+                        "Cari Tampat Ngopimu Sekarang !",
+                        style: GoogleFonts.sora(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        left: 16,
+                        right: 16,
+                        bottom: 8,
+                      ),
+                      child: Text(
+                        "Untuk Menikamati Fitur Kami Anda Perlu trhubung Dahulu Ke iNTERNET",
+                        textAlign: TextAlign.center,
+                        style: GoogleFonts.sora(
+                          color: Colors.white,
+                          fontSize: 12,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        left: 16,
+                        right: 16,
+                        bottom: 32,
+                      ),
+                      child: Card(
+                        child: Padding(
+                          padding: const EdgeInsets.all(14.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Image.asset(
+                                "assets/images/google.jpg",
+                                width: 40,
+                              ),
+                              SizedBox(
+                                width: 30,
+                              ),
+                              Text(
+                                "Login Sekarang",
+                                style: GoogleFonts.sora(
+                                  color: Colors.black,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
